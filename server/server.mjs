86d69a01b3824a33b03b42a545834fe490d30a85
Neli Bishka <bishkaneli@gmail.com>
2024-07-10 17:57:03 +0200

    path fix

--- server/server.mjs
+++ server/server.mjs
@@ -112,7 +112,7 @@ app.post('/capture-checkout', (req, res) => {
 app.use(express.static(path.join(__dirname, './build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build', 'index.html'));
+  res.sendFile(path.join(__dirname, './mb_eCommerce/build', 'index.html'));
 });
 
 app.listen(port, () => {
